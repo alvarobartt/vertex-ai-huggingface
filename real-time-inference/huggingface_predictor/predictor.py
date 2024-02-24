0c5370eb7a16b736fe80dda16e54b372f79df5f8
Alvaro Bartolome <alvaro@argilla.io>
2024-02-24 18:53:25 +0100

    Fix `huggingface_predictor/predictor.py` formatting

--- real-time-inference/huggingface_predictor/predictor.py
+++ real-time-inference/huggingface_predictor/predictor.py
@@ -15,18 +15,18 @@ logger.setLevel(logging.DEBUG)
 class HuggingFacePredictor(Predictor):
     def __init__(self) -> None:
         pass
-    
+
     def load(self, artifacts_uri: str) -> None:
         """Loads the preprocessor and model artifacts."""
         logger.debug(f"Downloading artifacts from {artifacts_uri}")
         prediction_utils.download_model_artifacts(artifacts_uri)
-        logger.debug(f"Artifacts successfully downloaded!")
+        logger.debug("Artifacts successfully downloaded!")
         os.makedirs("./model", exist_ok=True)
         with tarfile.open("model.tar.gz", "r:gz") as tar:
             tar.extractall(path="./model")
         logger.debug(f"HF_TASK value is {os.getenv('HF_TASK')}")
-        self._pipeline = pipeline(os.getenv("HF_TASK", None), model="./model")
-        logger.debug(f"`pipeline` successfully loaded!")
+        self._pipeline = pipeline(os.getenv("HF_TASK", ""), model="./model")  # type: ignore
+        logger.debug("`pipeline` successfully loaded!")
 
     def predict(self, instances: Dict[str, Any]) -> Dict[str, Any]:
-        return self._pipeline(**instances)
+        return self._pipeline(**instances)  # type: ignore
