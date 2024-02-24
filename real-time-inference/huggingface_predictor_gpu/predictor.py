e1f7d18b121c14ddb09f6c323fa8fb8f69d6e8fb
Alvaro Bartolome <alvaro@argilla.io>
2024-02-24 18:53:08 +0100

    Add `05-custom-inference-on-gpu.ipynb`

---
+++ real-time-inference/huggingface_predictor_gpu/predictor.py
@@ -0,0 +1,33 @@
+import os
+import logging
+import tarfile
+from typing import Any, Dict
+
+from transformers import pipeline
+
+from google.cloud.aiplatform.prediction.predictor import Predictor
+from google.cloud.aiplatform.utils import prediction_utils
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class HuggingFacePredictor(Predictor):
+    def __init__(self) -> None:
+        pass
+
+    def load(self, artifacts_uri: str) -> None:
+        """Loads the preprocessor and model artifacts."""
+        logger.debug(f"Downloading artifacts from {artifacts_uri}")
+        prediction_utils.download_model_artifacts(artifacts_uri)
+        logger.debug("Artifacts successfully downloaded!")
+        os.makedirs("./model", exist_ok=True)
+        with tarfile.open("model.tar.gz", "r:gz") as tar:
+            tar.extractall(path="./model")
+        logger.debug(f"HF_TASK value is {os.getenv('HF_TASK')}")
+        self._pipeline = pipeline(os.getenv("HF_TASK", ""), model="./model", device_map="auto")  # type: ignore
+        logger.debug("`pipeline` successfully loaded!")
+        logger.debug(f"`pipeline` is using device={self._pipeline.device}")
+
+    def predict(self, instances: Dict[str, Any]) -> Dict[str, Any]:
+        return self._pipeline(**instances)  # type: ignore
